8547b9087209d8646e9ec478fb08590f067c19f7
purplenaive <59996628+purplenaive@users.noreply.github.com>
2022-11-24 17:02:01 +0900

    프론트 vue로 구성
    - vue로 프론트 구성 후 폴더별로 front, back 분리
    - 메인 및 작성 페이지 작성 필요

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main-header></main-header>
+  <router-view />
+</template>
+
+<script>
+  import mainHeader from './components/mainHeader.vue';
+
+  export default {
+    name: "App",
+    components: { mainHeader, }
+  }
+</script>
+
+<style lang="scss">
+  @import "@/assets/scss/common";
+
+  html {
+    font-size: 16px;
+    font-family: $font;
+  }
+</style>
---
+++ frontend/src/components/mainHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="component main-header">
+    <nav class="main-nav">
+      <router-link 
+        v-for="nav in menu"
+        :key="nav.title"
+        :to="{ name: nav.path }"
+      >{{nav.title}}</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+  import { toRefs, reactive } from 'vue';
+
+  export default {
+    name: "mainHeader",
+    setup() {
+      const state = reactive({
+        menu: [
+          { title: "Home", path: "home" },
+          { title: "Write", path: "home" },
+        ],
+      })
+
+      return {
+        ...toRefs(state),
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .main-header {
+    height: 40px;
+    padding: 0 24px;
+    background-color: $primary-main;
+  }
+</style>
