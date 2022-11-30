229d22e4354931e0be1130eb6079a32e13dc40b4
purplenaive <59996628+purplenaive@users.noreply.github.com>
2022-11-30 17:24:02 +0900

    db에 연결하여 method 수행
    - 데이터 베이스에 데이터 삽입 동작 연습
    - 데이터 베이스에서 데이터 가져오기 연습

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const app = express();
+const mongo_client = require("mongodb").MongoClient;
+var db;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.listen(3000, function() {
-  console.log("listening on 3000");
-});
+mongo_client.connect("mongodb+srv://purplenaive:woojisoo7321@cluster0.mproxno.mongodb.net/?retryWrites=true&w=majority", function(error, client) {
+  if(error) return console.log(error);
+
+  db = client.db("todo");
+
+  app.listen(3000, function() {
+    console.log("listening on 3000");
+  });
+})
+
 
 // 누군가가 /pet으로 방문을 하면 pet 관련된 안내문을 띄워주자
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
+// get todo list
+app.get("/api/todos", function(request, response) {
+  db.collection("post").find().toArray(function(error, result) {
+    response.send(result);
+  })
+});
+
+// add todo item
 app.post("/api/add", function(request, response) {
-  console.log(request.body);
+  const { title, date } = request.body;
+
+  db.collection("post").insertOne({ title, date }, function(error, result) {
+    if(error) console.log("data save in post got error");
+  })
+
   response.send("complete");
 });
--- frontend/src/views/homeView.vue
+++ frontend/src/views/homeView.vue
@@ -1,5 +1,31 @@
 <template>
   <main class="page home-page">
-    hello
+    {{todos}}
   </main>
 </template>
+
+<script>
+  import { reactive, toRefs } from 'vue';
+  import axios from 'axios';
+
+  export default {
+    name: "homeView",
+
+    setup() {
+      const state = reactive({
+        todos: [],
+      })
+      // todo 가져오기
+      const getTodos = async function() {
+        const data = await axios.get("/api/todos");
+        console.log(data);
+        state.todos = data.data;
+      };
+      getTodos();
+
+      return {
+        ...toRefs(state),
+      }
+    }
+  }
+</script>
